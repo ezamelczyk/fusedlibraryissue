3b1e2c77a6758219e3f45cfd2908aac38a7b99e0
Ernest Zamelczyk <ernest.zamelczyk@neptis.pl>
2025-06-26 14:33:40 +0200

    Added another failing example

--- fusedlibrary/build.gradle.kts
+++ fusedlibrary/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.attributes.BuildTypeAttr
+
 plugins {
     alias(libs.plugins.android.fusedlibrary)
 }
@@ -8,5 +10,20 @@ androidFusedLibrary {
 }
 
 dependencies {
-    include("io.grpc:grpc-protobuf:1.73.0")
+    include(project(":mylibrary"))
+}
+
+// Temporary workaround for b/425861331, remove when issue is fixed.
+configurations.configureEach {
+    attributes {
+        // This attribute can be removed from AGP 8.12-alpha07.
+        attribute(
+            BuildTypeAttr.ATTRIBUTE,
+            project.objects.named(BuildTypeAttr::class.java, "release"),
+        )
+        attribute(
+            TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+            project.objects.named(TargetJvmEnvironment::class.java, TargetJvmEnvironment.ANDROID)
+        )
+    }
 }
